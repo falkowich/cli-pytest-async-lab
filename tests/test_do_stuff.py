6cb636461f1b93d98de8a7f3db52ff84ed8691ed
Falk <andreas@mockel.se>
2021-08-31 20:27:42 +0200

    computer change, push

--- tests/test_do_stuff.py
+++ tests/test_do_stuff.py
@@ -1,4 +1,5 @@
 import do_stuff
+import os
 
 # test isolated function
 def test_main_set_bar():
@@ -15,3 +16,10 @@ def test_main_get_foo_output(capsys):
     output = do_stuff.get_foo("foo")
     captured = capsys.readouterr()
     assert captured.out == "foo\n"
+
+
+def test_main_help_output(capsys):
+    os.sys.argv = ["--help"]
+    res = do_stuff.main()
+    captured = capsys.readouterr()
+    assert captured.out == 0
